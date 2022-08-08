5e6f286bdc8603150b31a7ae9a58bd05babf6be1
Mostak Shahariyar <mostakshahariyar18@gmail.com>
2022-08-09 01:24:40 +0600

    done

--- ema-jon-react/src/Components/Shop/Shop.js
+++ ema-jon-react/src/Components/Shop/Shop.js
@@ -30,8 +30,19 @@ const Shop = () => {
         }
     },[products])
 
-    const hendelToAddCart = product =>{
-        const newArray = [...cart, product]
+    const hendelToAddCart = product => {
+        const exists = cart.find(pd => pd.key === product.key);
+        console.log(exists)
+        let newArray = [];
+        if (exists) {
+            const rest = cart.filter(pd => pd.key !== product.key);
+            exists.quentity = exists.quentity + 1;
+            newArray = [...rest, product]
+        }
+        else {
+            product.quentity = 1; 
+            newArray = [...cart, product]
+        }
         setCart(newArray)
         addToDb(product.key)
     }
